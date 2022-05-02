2ea067231bf57a6b2677eceb95712c058882ceac
jhonv4sq <jhonv4sq.01@gmail.com>
2022-05-02 15:56:19 +0200

    axios installed

--- src/tailwind_components/TailwindHello.vue
+++ src/tailwind_components/TailwindHello.vue
@@ -1,13 +1,22 @@
 <template>
     <h1 class="text-3xl font-bold underline">
-        Hola mundo!
+        
     </h1>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
-}
+  data: () => ({
+    result: null
+  }),
+  created() {
+    axios.get("https://opentdb.com/api.php?amount=10").then((result) => {
+      this.result = result.data;
+      console.log(result);
+    })
+  }
+};
 </script>
 
 <style>
