02d912a2e01246aa8e1accffe98cdbd5629d480e
jhonv4sq <jhonv4sq.01@gmail.com>
2022-05-02 11:55:59 +0200

    tailwind installed

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import TailwindHello from './tailwind_components/TailwindHello.vue'
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
   </header>
 
-  <main>
-    <TheWelcome />
+  <main>    
+    <tailwind-hello />
   </main>
+
 </template>
 
 <style>
