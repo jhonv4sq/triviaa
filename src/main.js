d2ca9c149b3a7ac826b45512a626669e7a72389e
Adrián Pelayo <niniohhns@gmail.com>
2022-05-04 15:41:05 +0200

    an uppercase letter

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { data } from 'autoprefixer';
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue'
-import Home from '@/views/home.vue';
+import Home from '@/views/Home.vue';
 import Settings from '@/views/Settings.vue';
 import './index.css'
 
