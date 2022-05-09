38309cd84ecdeb9a40d57f30d49d87f99bcb63b2
jhonv4sq <jhonv4sq.01@gmail.com>
2022-05-09 12:57:30 +0200

    update 09/05/2022

--- src/tailwind_components/Question.vue
+++ src/tailwind_components/Question.vue
@@ -1,21 +1,26 @@
 <template>
-  <div class="flex items-center justify-center text-center px-2 font-hammer h-32 bg-primary w-80 md:w-96 rounded">
-      <label for="Question">{{ this.object.question }}</label>
+  <div
+    class="flex items-center justify-center text-center px-2 font-hammer h-32 bg-primary w-80 md:w-96 rounded"
+  >
+    <label for="Question">{{ this.object.question }}</label>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Question',
-    props:{
-        object:{
-            type: Object,
-            default: () => ({})
-        }
-    }    
-}
+  name: 'Question',
+  props: {
+    object: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  async data() {
+    return {
+      question: this.object.question,
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
