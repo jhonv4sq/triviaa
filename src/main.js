5dd290d32c7b5cb1c9371afa5c2fd949a0b3c33a
Jhon <jhonv4sq.01@gmail.com>
2024-08-16 00:17:52 +0200

    Add views

--- src/main.js
+++ src/main.js
@@ -1,5 +1,36 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createWebHistory, createRouter } from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = [
+  { 
+    path: '/', 
+    name: 'home',
+    component: () => import('./views/Home.vue')
+  },
+  { 
+    path: '/game', 
+    name: 'game',
+    component: () => import('./views/Game.vue')
+  },
+  { 
+    path: '/settings', 
+    name: 'settings',
+    component: () => import('./views/Settings.vue')
+  },
+  { 
+    path: '/score', 
+    name: 'score',
+    component: () => import('./views/Score.vue')
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
---
+++ src/views/Game.vue
@@ -0,0 +1,15 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: 'Game',
+    data() {
+
+    },
+    methods: {
+        
+    }
+}
+</script>
