d30a10bcdbdf0e6d7908266c76717ab952de4e17
Jhon Erick <jhonv4sq.01@gmail.com>
2024-08-15 23:26:07 +0200

    Replace folders

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { fileURLToPath, URL } from 'url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://jhonv4sq.github.io/triviaa/",
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
 })
